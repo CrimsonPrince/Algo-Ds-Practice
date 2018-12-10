b538c540e9791589edeca580555ea4de0f92c302
CrimsonPrince <CrimsonPrince@protonmail.com>
2018-12-10 21:32:13 +0000

    Added CSV's and Code

--- mongodbImport.py
+++ mongodbImport.py
@@ -1,15 +1,13 @@
-import pymongo
+from pymongo import MongoClient
+import pandas as pd
+import pprint
+client = MongoClient()
+db=client.test
+games = db.games
+df = pd.read_csv("ign.csv") #csv file which you want to import
+records_ = df.to_dict(orient = 'records')
+#print(records_[1])
+result = db.employee.insert_many(records_)
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-
-mydb = myclient["olympics"]
-
-print(myclient.list_database_names())
-
-participant = mydb['participant']
-
-import csv
-with open('ign.csv', newline='') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for i in range(10):
-		print(reader[i])
+x = db.employee.find_one()
+print(x)
