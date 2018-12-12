803b6f8eab123d36305835ae8226ec9476048873
CrimsonPrince <CrimsonPrince@protonmail.com>
2018-12-12 00:32:08 +0000

    Cracked the code, Mongodb such a pain sometimes

--- mongoDbQuery.py
+++ mongoDbQuery.py
@@ -1,9 +1,16 @@
-import pprint
+from pprint import pprint
 from pymongo import MongoClient
 
 client = MongoClient()
-db=client.olympics
-olympics = db.games
+db=client.airplane_crashes
+cursor = db.main2
 
-result = olympics.find()
-i = 0
+result = cursor.find()
+
+y = 0
+data = {}
+
+result = cursor.find({},{'_id' : 0})
+
+for row in result:
+	print(row)
--- mongoNewImport.py
+++ mongoNewImport.py
@@ -6,4 +6,16 @@ db=client.airplane_crashes
 
 df = pd.read_csv("Airplane_Crashes_and_Fatalities_Since_1908.csv") #csv file which you want to import
 records_ = df.to_dict(orient = 'records')
-#print(records_[1])
+for key, items in records.items():
+	print(items['Date'])
+
+"""data = {}
+plane = {}
+
+data['Date'] = 12
+data['Name'] = "Arthur"
+plane['age'] = 10
+plane['Year'] = 1194
+data['Plane'] = plane
+
+result = db.main2.insert_one(data)"""
