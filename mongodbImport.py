9ea61eb0642cd9949ecaf4be1a7a1dd72dd91d4e
Arthur(Crimson) <Kingr4@protonmail.ch>
2018-12-12 03:16:49 +0000

    Actually made it work, _id is really annoying.

--- mongodbImport.py
+++ mongodbImport.py
@@ -5,5 +5,6 @@ client = MongoClient()
 db=client.airplane_crashes
 
 df = pd.read_csv("Airplane_Crashes_and_Fatalities_Since_1908.csv") #csv file which you want to import
-records_ = df.to_dict(orient = 'records')
-#print(records_[1])
+records_ = df.to_dict(orient = 'record')
+result = db.main.insert_many(records_)
+print(result.inserted_ids)
