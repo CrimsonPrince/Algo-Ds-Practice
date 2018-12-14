17b119b99fe04d1fefa00b380ecc48fc8883c3dc
CrimsonPrince <CrimsonPrince@protonmail.com>
2018-12-14 22:14:26 +0000

    Updating Format of DB to one to few

--- mongoNewImport.py
+++ mongoNewImport.py
@@ -10,12 +10,12 @@ if client.drop_database('airplane_crashes'):
 
 df = pd.read_csv("Airplane_Crashes_and_Fatalities_Since_1908.csv") #csv file which you want to import
 records = df.to_dict(orient = 'records')
-insertionList = list()
 
 for items in records:
 	crash = {}
-	deaths = {}
+	plane = {}
 	operator = {}
+	planeList = []
 
 	crash['flight_Number'] = items['Flight #']
 	crash['route'] = items['Route']
@@ -23,26 +23,25 @@ for items in records:
 	crash['aboard'] = items['Aboard']
 	crash['summary'] = items['Summary']
 
-	crash['type'] = items['Type']
-	crash['cnin'] = items['cn/In']
-	crash['registration'] = items['Registration']
+	plane['type'] = items['Type']
+	plane['cnin'] = items['cn/In']
+	plane['registration'] = items['Registration']
+	planeList.append(plane)
+	crash['Plane'] = planeList
 	operator['crash'] = crash
 
 	crash['date'] = items['Date']
 	crash['Time'] = items['Time']
 	crash['location'] = items['Location']
-	deaths['in_Air'] = items['Fatalities']
-	deaths['on_Ground'] = items['Ground']
+	crash['in_Air'] = items['Fatalities']
+	crash['on_Ground'] = items['Ground']
 
-	crash['deaths'] = deaths
 	operator['crash'] = crash
 
-	insertionList.append(operator)
-
-try:
-	db.newFormat.insert_many(insertionList)
-except pymongo.errors.BulkWriteError as e:
-	print(e.details['writeErrors'])
+	try:
+		db.newFormat.insert(operator)
+	except pymongo.errors.BulkWriteError as e:
+		print(e.details['writeErrors'])
 
 #for i in insertionList:
 	#print(i)
