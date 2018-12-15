ba2b87984988951bf5040afdbfd68b0e1108ebe3
Arthur(Crimson) <Kingr4@protonmail.ch>
2018-12-15 02:24:30 +0000

    MongoEngine is meh as well

--- MongoEngineTest.py
+++ MongoEngineTest.py
@@ -2,14 +2,14 @@ from pprint import pprint
 from mongoengine import *
 import pandas as pd
 
-class Crash(EmbeddedDocument):
+class Operator(Document):
+	name = StringField(max_length=20000)
+
+class Crash(Document):
 	Type = StringField(max_length=20000)
 	Date = StringField(max_length=20000)
 	Location = StringField(max_length=20000)
-
-class Operator(Document):
-	name = StringField(max_length=20000)
-	crashes = ListField(EmbeddedDocumentField(Crash))
+	OperatorName = ReferenceField(Operator)
 
 
 connect('airplane_crashes')
@@ -18,15 +18,17 @@ connect('airplane_crashes')
 df = pd.read_csv("Airplane_Crashes_and_Fatalities_Since_1908.csv") #csv file which you want to import
 df['Operator'] = df['Operator'].fillna("Unknown")
 records = df.to_dict(orient = 'records')
-
 operatorDict = {}
 
-for row in records:
-	operator = Operator(name = row['Operator'])
-	operator.save()
 
+for row in records:
 	if row['Operator'] not in operatorDict:
+		operator = Operator(name = row['Operator'])
+		operator.save()
 		operatorDict[row['Operator']] = list()
 
-for operator in Operator.objects:
-	print(operator.name)
+	crash = Crash(Type = str(row['Type']), Date = str(row['Date']), Location = str(row['Location']), OperatorName = row['Operator'])
+	crash.save()
+
+"""for operator in Operator.objects:
+	pprint(Crash.objects(OperatorName__all=[]))"""
