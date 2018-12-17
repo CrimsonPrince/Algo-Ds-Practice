7d757e7dd066fc4ac61944ec343ae48dd4f94d1b
Arthur(Crimson) <Kingr4@protonmail.ch>
2018-12-17 16:45:37 +0000

    Submitted.

--- mongoDbQuery.py
+++ mongoDbQuery.py
@@ -13,7 +13,7 @@ data = {}
 
 #result = cursor.find({},{'_id' : 0})
 #result = cursor.find({'Operator':'Military - U.S. Army'},{'_id' : 0})
-result = cursor.find({'Crash.cnin': '47'}, {'Crash.cnin':1})
+result = cursor.find({'Crash.cnin': '47'}, {'Crashes.cnin':1})
 result = cursor.find({ "Crash" : { "$elemMatch" : { "cnin" : "47"} }})
 
 agr = [ {'$group': {'_id': 1, 'all': { '$sum': '$Crash.in_Air' } } } ]
@@ -21,15 +21,9 @@ agr = [ {'$group': {'_id': 1, 'all': { '$sum': '$Crash.in_Air' } } } ]
 val = list(db.newFormat.aggregate(agr))
 
 
-db.newFormat.find(
-	{
-		'Crash.cnin': '47'
-	},
-	{
-		'Crash.cnin.$': 1,
-		 name: 1
-	}
-)
+db.newFormat.find({'Crashes.cnin': '47'},{'Crashes.cnin.$': 1, name: 1})
+
+db.newFormat.find({}, {'Operator':'Military - U.S. Army'})
 
 db.newFormat.aggregate([{$group : {_id : "$Crash.type", num_tutorial : {$sum : "$Crash.in_Air"}}}]
 db.newFormat.aggregate([{$group : {_id : "$Type", num_tutorial : {$avg : "$in_Air"}}}])
@@ -43,6 +37,8 @@ db.newFormat.aggregate([{$group : {_id : "$Type", num_tutorial : {$avg : "$in_Ai
 
 
 db.newFormat.aggregate( [{ $unwind: "$Crashes" },{$group: {_id: {title: '$type'},SumOfDeaths: { $sum: '$Crashes.in_Air' }}},{$project: {_id: 0,title: '$_id.type',SumOfDeaths: 1}}])
+
+db.newFormat.aggregate( [{ $unwind: "$Crashes" },{$group: {_id: {title: '$type'},SumOfDeaths: { $avg: '$Crashes.in_Air' }}},{$project: {_id: 0,title: '$_id.type',SumOfDeaths: 1}}])
 #print('The sum of prices is {}'.format(val[0]['all']))
 #result = cursor.find({"Plane.type":"Zeppelin L-53 (airship)"})
 
