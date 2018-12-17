ba6b20bc6a730dc983db711972b0a5fd63c9c16b
Arthur(Crimson) <Kingr4@protonmail.ch>
2018-12-17 00:08:36 +0000

    Finished MongoDB queries

--- mongoDbQuery.py
+++ mongoDbQuery.py
@@ -13,13 +13,37 @@ data = {}
 
 #result = cursor.find({},{'_id' : 0})
 #result = cursor.find({'Operator':'Military - U.S. Army'},{'_id' : 0})
-#result = cursor.find({'Operator':'Military - U.S. Army'},{'in_Air' : {'$gte':30}})
+result = cursor.find({'Crash.cnin': '47'}, {'Crash.cnin':1})
+result = cursor.find({ "Crash" : { "$elemMatch" : { "cnin" : "47"} }})
 
-agr = [ {'$group': {'_id': 1, 'all': { '$sum': '$in_Air' } } } ]
+agr = [ {'$group': {'_id': 1, 'all': { '$sum': '$Crash.in_Air' } } } ]
 
 val = list(db.newFormat.aggregate(agr))
 
-print('The sum of prices is {}'.format(val[0]['all']))
+
+db.newFormat.find(
+	{
+		'Crash.cnin': '47'
+	},
+	{
+		'Crash.cnin.$': 1,
+		 name: 1
+	}
+)
+
+db.newFormat.aggregate([{$group : {_id : "$Crash.type", num_tutorial : {$sum : "$Crash.in_Air"}}}]
+db.newFormat.aggregate([{$group : {_id : "$Type", num_tutorial : {$avg : "$in_Air"}}}])
+
+
+#db.newFormat.aggregate([{$match: {"Operator": "Military - U.S. Army"} },{$unwind: '$Crash'},{$group: {_id: null, "Sum": {$sum: "$Crash.in_Air" }}}])
+
+
+#db.collection.aggregate( [{ $unwind: "$Crash" },{$group: {_id: {title: '$type'},SumOfDeaths: { $sum: '$crash.in_Air' }}},{$project: {_id: 0,title: '$_id.type',SumOfDeaths: 1}}]);
+
+
+
+db.newFormat.aggregate( [{ $unwind: "$Crashes" },{$group: {_id: {title: '$type'},SumOfDeaths: { $sum: '$Crashes.in_Air' }}},{$project: {_id: 0,title: '$_id.type',SumOfDeaths: 1}}])
+#print('The sum of prices is {}'.format(val[0]['all']))
 #result = cursor.find({"Plane.type":"Zeppelin L-53 (airship)"})
 
 #pipeline = [{"$match": {"operator": "Aer Lingus"}}, { "$group": {"_id": "$aboard","TotalDeaths": { "$sum": "$total" }}}]
@@ -27,8 +51,8 @@ print('The sum of prices is {}'.format(val[0]['all']))
 
 #result = cursor.find_one({},{'_id' : 0})
 
-#result2 = list(result)
-#pprint(result2)
+result2 = list(result)
+pprint(result2)
 
 #for row in result:
 	#pprint(row)
