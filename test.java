034d8945cade379b34c1ab54c07892d4813a54a8
Arthur(Crimson) <Kingr4@protonmail.ch>
2019-01-06 15:48:35 +0000

    Testing Complete

---
+++ test.java
@@ -0,0 +1,17 @@
+public class test {
+
+	public test(){
+
+	}
+
+	public static void main(String []args)
+	{
+		test object = new test();
+		System.out.println(object.method());
+	}
+
+	public int method()
+	{
+		int i = 1 + 1;
+	}
+}
